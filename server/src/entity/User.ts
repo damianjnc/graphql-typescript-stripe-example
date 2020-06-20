cc37d55be5ba1b380232567ab5b0dac052ae43bf
Damian <damianjnc@gmail.com>
2020-06-20 06:39:12 +0000

    Updates

---
+++ server/src/entity/User.ts
@@ -0,0 +1,25 @@
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from "typeorm"
+
+@Entity("users")
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column("int", { default: 0 })
+  count: number
+
+  @Column("text")
+  email: string
+
+  @Column("text", { nullable: true })
+  stripeId: string | null
+
+  @Column("text", { default: "free-trial" })
+  type: string
+
+  @Column("text", { nullable: true })
+  ccLast4: string | null
+
+  @Column("text")
+  password: string
+}
---
+++ server/src/index.ts
@@ -0,0 +1,85 @@
+import "reflect-metadata"
+import "dotenv/config"
+import { createConnection } from "typeorm"
+import { ApolloServer } from "apollo-server-express"
+import * as express from "express"
+import * as cookieParser from "cookie-parser"
+
+import { typeDefs } from "./typeDefs"
+import { resolvers } from "./resolvers"
+import { verify } from "jsonwebtoken"
+import { ACCESS_TOKEN_SECRET, REFRESH_TOKEN_SECRET } from "./constants"
+import { User } from "./entity/User"
+import { createTokens } from "./auth"
+
+const startServer = async () => {
+  const server = new ApolloServer({
+    // These will be defined for both new or existing servers
+    typeDefs,
+    resolvers,
+    context: ({ req, res }: any) => ({ req, res })
+  })
+
+  await createConnection()
+
+  const app = express()
+
+  app.use(cookieParser())
+
+  app.use(async (req: any, res, next) => {
+    const refreshToken = req.cookies["refresh-token"]
+    const accessToken = req.cookies["access-token"]
+
+    if (!refreshToken && !accessToken) {
+      return next()
+    }
+
+    try {
+      const data = verify(accessToken, ACCESS_TOKEN_SECRET) as any
+      req.userId = data.userId
+      return next()
+    } catch {}
+
+    if (!refreshToken) {
+      return next()
+    }
+
+    let data
+
+    try {
+      data = verify(refreshToken, REFRESH_TOKEN_SECRET) as any
+    } catch {
+      return next()
+    }
+
+    const user = await User.findOne(data.userId)
+    // token has been invalidated
+    console.log("user", user)
+    console.log("data", data)
+    if (!user || user.count !== data.count) {
+      return next()
+    }
+
+    console.log("user2", user)
+    const tokens = createTokens(user)
+    res.cookie("refresh-token", tokens.refreshToken)
+    res.cookie("access-token", tokens.accessToken)
+    req.userId = user.id
+
+    next()
+  })
+
+  server.applyMiddleware({
+    app,
+    cors: {
+      credentials: true,
+      origin: "http://localhost:3000"
+    }
+  }) // app is from an existing express app
+
+  app.listen({ port: 4000 }, () =>
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  )
+}
+
+startServer()
